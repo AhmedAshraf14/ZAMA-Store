89d7c6231645da1ac73eb39f5d211a533670c8aa
Ashroof0 <Ashraf.mohamed.sh@gmail.com>
2024-09-07 15:30:18 +0300

    adding Scene deleagte

---
+++ ZAMAStore/Modules/CategoriesModule/CategoriesView/ProductCell/View/ProductItem.swift
@@ -0,0 +1,21 @@
+//
+//  ProductItem.swift
+//  ZAMAStore
+//
+//  Created by zyad Baset on 06/09/2024.
+//
+
+import UIKit
+
+class ProductItem: UICollectionViewCell {
+
+    @IBOutlet weak var lblPrice: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var view: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ ZAMAStore/Modules/CategoriesModule/CategoriesView/ProductCell/ViewModel/ProductCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductCellViewModel.swift
+//  ZAMAStore
+//
+//  Created by zyad Baset on 06/09/2024.
+//
+
+import Foundation
