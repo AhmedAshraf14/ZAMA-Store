c99708af07b11092ed8f765e8c02196d01335f24
AhmedAshraf14 <a.ashraf.dev@gmail.com>
2024-09-14 17:39:58 +0300

    enhancments for brands cell and ordersModel

--- ZAMAStore/Modules/BrandModule/BrandView/BrandCell/BrandCollectionViewCell.swift
+++ ZAMAStore/Modules/BrandModule/BrandView/BrandCell/BrandCollectionViewCell.swift
@@ -15,17 +15,19 @@ class BrandCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var brandName: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        setupShadow()
+//        setupShadow()
     }
-    private func setupShadow(){
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 5)
-        self.layer.shadowRadius = 4
-        self.layer.shadowOpacity = 0.5
-        self.layer.masksToBounds = false
-    }
+//    private func setupShadow(){
+//        self.layer.shadowColor = UIColor.black.cgColor
+//        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+//        self.layer.shadowRadius = 4
+//        self.layer.shadowOpacity = 0.5
+//        self.layer.masksToBounds = false
+//    }
     override func layoutSubviews() {
         super.layoutSubviews()
         self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.layer.cornerRadius).cgPath
+        self.layer.borderColor = UIColor.mintGreen.cgColor
+        self.layer.borderWidth = 1
     }
 }
